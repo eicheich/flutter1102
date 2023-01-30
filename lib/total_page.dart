c02d130af47fbe439a118862ef8463e65a85b425
WilliAndHan <william.a.hanantha@gmail.com>
2023-01-30 09:20:07 +0700

    Feat : [WA] add state management

---
+++ lib/total_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'controller.dart';
+
+class TotalPage extends StatelessWidget {
+  //inisialisasi
+  final Controller c = Get.put(Controller());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new_outlined,
+            color: Colors.white,
+          ),
+          onPressed: () => Get.back(),
+        ),
+        title: Text("Total Product"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Text(
+              "Total Product",
+              style: TextStyle(fontSize: 25),
+            ),
+            Text(
+              //mengambil hasil perhitungan dari tomat dan cabbage
+              c.sumTotal().toString(),
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
